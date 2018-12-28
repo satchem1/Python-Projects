329f73911d3cf91bbce130c378a4f879e4d71290
satchem1 <satrajit.chakrabarty@gmail.com>
2018-12-28 22:36:22 +0100

    -started MassSpecAnalysis

---
+++ Python-Projects/Test001.py
@@ -0,0 +1,19 @@
+class MassSpecAnalysis:
+    def import_raw_file(self, file):
+        reader = open(file)
+        pass
+
+    def clean_tof_spectra(self):
+        pass
+
+    def pick_peaks_for_calibration(self):
+        pass
+
+    def calibrate_tof_spectra(self):
+        pass
+
+    def determine_peak_shape(self):
+        pass
+
+    def determine_number_concentration(self):
+        pass
